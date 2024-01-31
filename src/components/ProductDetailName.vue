8b10e126380fdeed49187f7608560498ddc640ae
Szymon Berski <cnk.one@gmail.com>
2024-01-31 16:05:28 +0100

    SDS-24-01: updated details component

--- src/components/ProductDetailName.vue
+++ src/components/ProductDetailName.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
+import { PropType } from 'vue';
+
 const props = defineProps({
+  as: {
+    type: String as PropType< 'th'| 'td' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'div' | 'span'>,
+    default: 'div',
+    required: true,
+  },
   text: {
     type: String,
     default: '',
@@ -14,11 +21,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="font-bold detail-name w-full sm:w-50 flex">
+  <component :is="props.as" class="font-bold detail-name w-full sm:w-50 flex">
     <span class="colon-after" :class="styles && styles.length ? styles : 'mt-auto'">
       {{ props.text }}
     </span>
-  </div>
+  </component>
 </template>
 
 
@@ -27,22 +34,23 @@ const props = defineProps({
   @apply overflow-hidden relative;
 
     span {
-      @apply bg-white dark:bg-blue-901 relative z-10 pr-1.5;
+      @apply block bg-white dark:bg-blue-901 relative z-10 pr-1.5 w-full;
+
+      &:before {
+        // order: 2;
+        @apply text-gray-300 absolute select-none border-b border-gray-200 dark:border-gray-600 w-full -z-1;
+        height: 1em;
+        white-space: nowrap;
+        font-weight: 100;
+        bottom: 3px;
+        left: 0;
+        flex: 1;
+        content: '';
+        position:absolute;
+      }
     }
 
-    &:after {
-      // order: 2;
-      @apply text-gray-300 absolute select-none border-b border-gray-200 dark:border-gray-600 w-full;
-      height: 1em;
-      white-space: nowrap;
-      font-weight: 100;
-      bottom: 3px;
-      left: 0;
-      flex: 1;
-      content: '';
-      position:absolute;
-    
-  }
+
 }
 
 </style>
---
+++ src/components/ProductDetailsList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import ProductDetailName from "./ProductDetailName.vue";
+const props = defineProps<{
+  data: string;
+}>();
+
+const theads = Object.keys(Object.values(props.data)[0])
+
+
+
+</script>
+
+<template>
+  <table class="details table-auto text-left bg-white w-full max-w-[800px]">
+    <tbody>
+      <tr class="border" v-for="row in props.data" :key="row">
+        <ProductDetailName as="th" :text="row.name" />
+        <td>{{ row.data }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss">
+  .details {
+    border: none;
+    box-shadow: none;
+
+    td,
+    tr,
+    th {
+      border: none;
+      @apply leading-4 text-sm;
+    }
+  }
+</style>
