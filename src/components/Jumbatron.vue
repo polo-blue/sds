e9f794a8c06820c2410b11e33a3726ae395a7d51
Szymon Berski <cnk.one@gmail.com>
2024-03-19 15:33:11 +0100

    Updated Jumbatron component

--- src/components/Jumbatron.vue
+++ src/components/Jumbatron.vue
@@ -15,17 +15,15 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="relative flex bg-vw " :class="props.small ? 'md:min-h-xs' : 'md:min-h-md'">
-    <header class="max-w-7xl mx-auto my-auto py-8 lg:w-full px-6 lg:px-8 text-center"  :class="props.small ? 'sm:py-12 md:py-14 lg:py-16 xl:py-20' : 'sm:py-16 md:py-20 lg:py-28 xl:py-32'">
-        <h1 class="text-3xl  tracking-tight font-headlight text-white sm:(text-4xl pt-0) md:text-5xl lg:text-6xl ">
-          <slot name="intro" />
-        </h1>
-        <slot name="subtitle" />
-        <div class="mt-5 sm:(mt-8 flex justify-center)" v-if="hasSlot('cta')">
-            <slot name="cta" />
-        </div>
-      </header>
-  </div>
+  <header class="relative flow content-grid bg-vw  mx-auto my-auto py-8 lg:w-full px-6 lg:px-8 text-center"  :class="props.small ? 'md:min-h-xs sm:py-12 md:py-14 lg:py-16 xl:py-20' : 'md:min-h-md sm:py-16 md:py-20 lg:py-28 xl:py-32'">
+      <h1 class="text-3xl  tracking-tight font-headlight text-white sm:(text-4xl pt-0) md:text-5xl lg:text-6xl ">
+        <slot name="intro" />
+      </h1>
+      <slot name="subtitle" />
+      <div class="mt-5 sm:(mt-8 flex justify-center)" v-if="hasSlot('cta')">
+          <slot name="cta" />
+      </div>
+    </header>
 </template>
 <style lang="scss">
 .bg-vw {
