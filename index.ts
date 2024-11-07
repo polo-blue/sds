6f191cff89ffedc36e375e44fb444459e2272888
Szymon <cnk.one@gmail.com>
2024-11-07 01:11:23 +0100

    Fixed default export

--- index.ts
+++ index.ts
@@ -1,6 +1,35 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro';
+// import MyComponent from './src/src/MyComponent.astro';
 
-export default MyComponent;
+
+
+// export default MyComponent;
+
+
+// src/index.ts
+
+// Vue Components
+export { default as FuckRussia } from './src/components/FuckRussia.vue';
+export { default as FlagPL } from './src/components/flags/FlagPL.vue';
+export { default as Badges } from './src/components/Badges.vue';
+export { default as SlimBanner } from './src/components/SlimBanner.vue';
+export { default as Jumbatron } from './src/components/Jumbatron.vue';
+export { default as Button } from './src/components/Button.vue';
+export { default as Breadcrumbs } from './src/components/Breadcrumbs.vue';
+export { default as ProductDetailsList } from './src/components/ProductDetailsList.vue';
+export { default as FeaturesList } from './src/components/FeaturesList.vue';
+export { default as ProductCodes } from './src/components/ProductCodes.vue';
+export { default as ProductEngineType } from './src/components/Product/ProductEngineType.vue';
+
+
+
+// Astro Components
+export { default as Copyright } from './src/components/Copyright.astro';
+export { default as HandDrive } from './src/components/HandDrive.astro';
+export { default as Faq } from './src/components/Faq.astro';
+export { default as FaqItem } from './src/components/FaqItem.astro';
+
+export { default as ProductNumber } from './src/components/Product/ProductNumber.astro';
+export { default as ProductImage } from './src/components/Product/ProductImage.astro';
