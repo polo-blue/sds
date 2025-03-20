093c5ea37b2192e5978decee5f291a9e131eaa14
Szymon <cnk.one@gmail.com>
2025-03-20 03:00:26 +0100

    SDS: fixed CategoryLink component

--- src/components/Category/CategoryLink.vue
+++ src/components/Category/CategoryLink.vue
@@ -2,7 +2,7 @@
     <div class="text-light-blue-400 uppercase text-base z-3 relative w-full">
       <template v-if="categories">
         <template v-for="(category, index) in categories" :key="category.uri">
-          
+          <a
             class="term-link text-sm sm:text-base"
             :class="{ 'before:content-empty before:w-px before:h-2.5 before:bg-gray-300 before:mx-2.5 before:inline-block before:relative': index > 0 }"
             :href="category.uri"
