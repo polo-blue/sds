eed1ca550b7d4a75afce2a94830aebcbd62d0062
Szymon <cnk.one@gmail.com>
2025-03-20 03:12:11 +0100

    SDS: updated category link

--- src/components/Category/CategoryLink.vue
+++ src/components/Category/CategoryLink.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="text-light-blue-400 uppercase text-base z-3 relative w-full">
-      <template v-if="categories">
-        <template v-for="(category, index) in categories" :key="category.uri">
-          <a
-            class="term-link text-sm sm:text-base"
-            :class="{ 'before:content-empty before:w-px before:h-2.5 before:bg-gray-300 before:mx-2.5 before:inline-block before:relative': index > 0 }"
-            :href="category.uri"
-          >
-            {{ category.name }}
-          </a>
-        </template>
-      </template>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'CategoryLink',
-    props: {
-      categories: {
-        type: Array,
-        default: () => []
-      }
+  <a
+    :href="href"
+    class="category-link"
+    :class="{ 'active': active }"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'CategoryLink',
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      required: true
     }
   }
-  </script>
+}
+</script>
