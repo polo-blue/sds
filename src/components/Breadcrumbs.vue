299b3634fde47dd72d27e961ff74a110d7e5acf7
Szymon Berski <cnk.one@gmail.com>
2025-08-19 11:18:27 +0200

    SDS 0.9.0: updated dependencies

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -41,51 +41,40 @@ const isLast = (index: number) => {
   <nav>
     <ul class="breadcrumbs-base">
       <li v-if="props.showBack" class="breadcrumb-item">
-        <button
-          class="breadcrumb-back-btn"
-          :title="props.textBack" 
-          onclick="history.back()"
-        >
+        <button class="breadcrumb-back-btn" :title="props.textBack"
+          onclick="history.back()">
           <span class="block px-3" i-bx-bx-arrow-back />
-        </button>
-      </li>
-    </ul>
-    <ul class="breadcrumbs-base overflow-x-auto overflow-y-hidden sm:mr-12" 
-        itemscope itemtype="https://schema.org/BreadcrumbList">
+          </button>
+        </li>
+      </ul>
+    <ul class="breadcrumbs-base overflow-x-auto overflow-y-hidden sm:mr-12" itemscope
+      itemtype="https://schema.org/BreadcrumbList">
       <li v-if="props.showHome" class="breadcrumb-item">
         <a href="/"
           class="breadcrumb-link flex items-center px-3 sm:px-0 py-4.25 sm:py-1 hover:text-brand-secondary whitespace-nowrap translate-y-0 text-sm my-auto"
-          :title="props.textBack" 
-          itemprop="item" 
-          i-carbon-home>
-        </a>
+          :title="props.textBack" itemprop="item" i-carbon-home>
+          </a>
+       
         <meta itemprop="position" content="1">
+       
       </li>
-      <li v-for="(crumb, index) in breadcrumbs" 
-          :key="index" 
-          class="breadcrumb-item" 
-          itemprop="itemListElement"
-          itemscope 
-          itemtype="https://schema.org/ListItem">
-        <!-- Add separator as template content instead of CSS ::before -->
+      <li v-for="(crumb, index) in breadcrumbs" :key="index" class="breadcrumb-item"
+        itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
         <span v-if="index > 0 || props.showHome" class="text-gray-400 px-1 py-4.25 sm:py-1">/</span>
-        
-        <a v-if="!isLast(index)" 
-           :href="crumb.path" 
-           class="breadcrumb-link" 
-           itemprop="item" 
-           :title="`Polo 6R ${crumb.name}`">
+
+        <a v-if="!isLast(index)":href="crumb.path"class="breadcrumb-link"
+         itemprop="item":title="`Polo 6R ${crumb.name}`">
           <strong class="font-normal" itemprop="name">{{ crumb.name }}</strong>
-        </a>
-        <a v-else 
-           :href="crumb.path"
-           class="breadcrumb-link breadcrumb-link-disabled"
-           :title="`Polo 6R ${crumb.name} ${productNumber}`">
-          <strong class="font-normal" itemprop="name" v-html="crumb.name" /> 
+          </a>
+        <span v-else class="breadcrumb-link breadcrumb-link-disabled"
+         :title="`Polo 6R ${crumb.name} ${productNumber}`" itemprop="item">
+          <strong class="font-normal" itemprop="name" v-html="crumb.name" />
           <b class="hidden sm:inline font-normal ml-1">{{ productNumber }}</b>
-        </a>
+          </span>
+       
         <meta itemprop="position" :content="String(props.showHome ? index + 2 : index + 1)">
+       
       </li>
-    </ul>
-  </nav>
+      </ul>
+    </nav>
 </template>
